16abe0bdf6b53fd30dad9d014d26276130a79520
Grace-Shao <gshao8@gatech.edu>
2025-02-15 03:24:13 -0500

    set up main pages to navigate to

---
+++ app/pages/searchPastVideosPage/page.tsx
@@ -0,0 +1,8 @@
+export default function SearchPastVideosPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-black">
+            <h1>SearchPastVideosPage</h1>
+            {/* Add your search functionality here */}
+        </main>
+    )
+}
